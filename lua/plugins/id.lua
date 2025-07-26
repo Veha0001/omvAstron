f75f24de664f8d71e27d6a07476c8394717b0b34
Veha Veha <vehav00@gmail.com>
2025-07-26 10:10:18 +0700

    Remove Non-LazyPlugin

--- lua/plugins/id.lua
+++ lua/plugins/id.lua
@@ -2,11 +2,8 @@ return {
   { import = "mods" },
   {
     "AstroNvim/astrocommunity",
-    { import = "astrocommunity.completion.blink-cmp" },
     { import = "astrocommunity.colorscheme.catppuccin" },
-    { import = "astrocommunity.indent.mini-indentscope" },
     { import = "astrocommunity.colorscheme.tokyonight-nvim" },
-    { import = "astrocommunity.markdown-and-latex.markview-nvim" },
     { import = "astrocommunity.utility.noice-nvim" },
   },
   {
